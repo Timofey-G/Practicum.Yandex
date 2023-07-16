cf5a2518edff4236d38bdf09a1f02f3779a75b3e
Тимофей Григоренко <yo.tgrig@yandex.ru>
2023-07-16 12:22:21 +0600

    Delete env

--- 2_Yatube/yatube/yatube/settings.py
+++ 2_Yatube/yatube/yatube/settings.py
@@ -1,8 +1,5 @@
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -12,17 +9,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "jjqi@d976w&s54^5s_up(&evnxdu+=pcff8(!jc!qx*wg0ek&2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "www.yatim.pythonanywhere.com",
-    "yatim.pythonanywhere.com",
-]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
